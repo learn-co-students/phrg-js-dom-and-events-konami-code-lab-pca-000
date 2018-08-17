6417dfc2ca22897e5aa3774e888a56a38bfbee02
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-08-16 20:34:44 -0400

    corrected the tests and my code

--- konami_code.js
+++ konami_code.js
@@ -1,21 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-const body = document.querySelector('body')
- body.addEventListener('keydown', function(e) {
-  return e.which
-})
+let body = document.body
+
 
 let index = 0
 
 function init() {
-}
+   body.addEventListener('keydown', function (e) {
+    const key = parseInt(e.detail || e.which);
+
   if (key === code[index]) {
     index++;
+
     if (index === code.length) {
-      alert("Hurray!")
+      window.alert("Hurray!");
     }
   }
   else {
     index = 0;
   }
-
+  })
 }
