31e9636f4140962a4918edd4c8bde39eecdfd9b5
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-08-16 20:14:05 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const body = document.querySelector('body')
+ body.addEventListener('keydown', function(e) {
+  return e.which
+})
+
+let index = 0
 
 function init() {
-  // Write your JavaScript code inside the init() function
+}
+  if (key === code[index]) {
+    index++;
+    if (index === code.length) {
+      alert("Hurray!")
+    }
+  }
+  else {
+    index = 0;
+  }
 
 }
